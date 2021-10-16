99485e9337ce9ada580936c51c586d3090c5f2f6
helaman-Y-B <helamashi@gmail.com>
2021-10-16 13:24:36 -0300

    Ignore this helama

--- jumper/game/puzzle.py
+++ jumper/game/puzzle.py
@@ -4,88 +4,87 @@
 # print guess a letter
 
 class Puzzle:
-    
+
     def __init__(self):
         """Initialize 
         """
-        
-        self.parachute_man = ["", "  ___", " /___\ ", " \   /", "  \ /", "   0", "  /|\ ", "  / \ ", "", "^^^^^^^", ""]
+
+        self.parachute_man = ["", "  ___", " /___\ ", " \   /",
+                              "  \ /", "   0", "  /|\ ", "  / \ ", "", "^^^^^^^", ""]
         # ~ for i in parachute_man:
-            # ~ print(i)
+        # ~ print(i)
         # ~ self.score = 4
         self.intents = 4
-            
+
     def set_word(self, word):
         """Sets the word to initialize the game interface.
         Args:
             word: the word chosen randomly.
         """
 
-        
-        
         self.word = word
         self.word_interface = ""
-        
+
         for _ in word:
             self.word_interface += "_ "
 
-        
-        
     def interface(self, lives):
         """It generates the game interface.
         Args:
             score: current score.
         """
 
-        
         if self.intents < lives:
-            
+
             self.parachute_man.pop(1)
-        
-        
+
         data = [self.word_interface, ]
-        
+
         for i in self.parachute_man:
             data.append(i)
-        
+
         return data
-        
-        
+
     def question(self):
         """It returns the game's question.
         """
 
-        
         main_question = "Guess a letter [a-z]: "
-        
+
         return main_question
-        
+
     def evaluate(self, letter):
         """It compares the current letter with the word and sets the new interface.
         Args:
             letter: current score.
         """
 
-        
         if letter in self.word:
             lister = list(self.word_interface)
-            index = self.word.index(letter)* 2
+            index = self.word.index(letter) * 2
             lister[index] = letter
             self.word_interface = "".join(lister)
             return True
-            
+
         else:
             self.intents -= 1
             return False
-            
-            
-            
-            
-        
-        
-        
 
-        
+    def question(self):
+        user_answer = self.question()
+
+        if user_answer == int:
+            print("The number was entered...\nPlease type a latter this time.")
+
+        elif user_answer == str:
+            alphabet = [
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+            ]
+            if user_answer in alphabet:
+                self.answer = True
+
+            elif user_answer in random_word:
+                self.answer = False
 
 
 starter = Puzzle()
@@ -94,8 +93,7 @@ starter.set_word("nicolas")
 print(starter.question())
 for i in starter.interface(4):
     print(i)
-    
+
 starter.evaluate("i")
 for i in starter.interface(4):
     print(i)
-
