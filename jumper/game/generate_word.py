eee897c8ec523a182f7f067bb1848c56f52343e2
James Charlie Salva <JamesSalvajames@hotmail.com>
2021-10-16 22:17:11 +0800

    I added a class template

---
+++ jumper/game/generate_word.py
@@ -0,0 +1,13 @@
+import random
+
+class Word:
+    """A code template for a random word generator. It will responsible for the randomly picking a word 
+    from a list or a file.
+    
+    Stereotype:
+        Information Holder
+
+    Attributes:
+        list_words (list): The list of words that will randomly pick a word from it.
+
+    """
